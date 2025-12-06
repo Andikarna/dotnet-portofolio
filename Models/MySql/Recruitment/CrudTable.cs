ad3275745ebf876f9b753802bc6f77f63f0c82ba
myusername <karnaandi00@gmail.com>
2025-12-06 10:02:04 +0700

    Update dotnet 9

--- Models/MySql/Recruitment/CrudTable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BasicProject.Models.MySql.Recruitment
-{
-    public partial class CrudTable
-    {
-        public int Id { get; set; }
-
-        public string? Data { get; set; }
-    }
-}
